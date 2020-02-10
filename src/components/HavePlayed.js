270cec560152dbbaa57a5a9f6d604ff9a7879a94
Xadr123 <hansenxander@gmail.com>
2020-02-10 12:07:20 -0700

    changed have played h3 title to completed

--- src/components/HavePlayed.js
+++ src/components/HavePlayed.js
@@ -8,7 +8,7 @@ class HavePlayed extends Component {
     render() {
         return (
             <div>
-                <h3 style={{ color: "white", fontSize: "40px" }}>Have Played</h3>
+                <h3 style={{ color: "white", fontSize: "40px" }}>Completed</h3>
                 {this.props.havePlayed.map(element => {
                     return <div style={{ background: "green", height: "40px", width: "400px", display: "flex", justifyContent: "space-around", fontWeight: "bolder", fontSize: "20px", alignItems: "center" }}>{element.name}
                         <button onClick={() => {
